ffc8b640b018a1389278a8a2d3a907266a5a5594
CalebMensah <mensahkhalib25@gail.com>
2026-06-18 10:04:34 +0000

    addd fix

--- app/(site)/privacy/page.tsx
+++ app/(site)/privacy/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function PrivacyPage() {
   return (
     <div className="min-h-screen bg-[#F8FAFC]">
--- app/(site)/terms/page.tsx
+++ app/(site)/terms/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function TermsPage() {
   return (
     <div className="min-h-screen bg-[#F8FAFC]">
